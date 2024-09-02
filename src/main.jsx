39cf3f22662398b3c126c73866a3d02072abdc3d
Kayla <kmnguyen436@gmail.com>
2024-09-02 16:16:30 -0500

    basename changes

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/reactjs-recipes-app/">
       <App />
     </BrowserRouter>
   </StrictMode>
